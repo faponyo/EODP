29831648416f6e6222c7ccf1379ed3073899fc04
faponyo <162140783+faponyo@users.noreply.github.com>
2025-09-08 13:42:49 +0300

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,31 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import { fileURLToPath, URL } from 'node:url'
+// import { webcrypto } from 'crypto' // ✅ ESM-compatible import
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+  return {
+    base: '/',
+    plugins: [react(),],
+    define: {
+      'process.env': {},
+      global: 'globalThis',
+    },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+      },
+    },
+    optimizeDeps: {
+      exclude: ['lucide-react'],
+    },
+    server: {
+      port: parseInt(env.VITE_PORT) || 5173, // 👈 change this to your desired port
+    },
+  }
+})
